9d277bf4403878fee2eb74462bead4e3c5a74a72
chinmaypshetty-wq <chinmay.p.shetty@gmail.com>
2026-01-12 22:03:15 +1030

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 # ==========================================
 # 1. CONFIGURATION
 # ==========================================
-MY_API_KEY = "AIzaSyBTF8y_wPlbRqYgkY7bYMEN_uRf-oePErs"
+MY_API_KEY = "AIzaSyC_I2Mrkmj98V66AnhIQfOCQyqtU1aOemQ"
 
 try:
     genai.configure(api_key=MY_API_KEY)
