da750e58f4537bdfa56e22cf7523dea667b6b8cf
chinmaypshetty-wq <chinmay.p.shetty@gmail.com>
2026-01-12 22:11:51 +1030

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 # ==========================================
 # 1. CONFIGURATION
 # ==========================================
-MY_API_KEY = "st.secrets["GOOGLE_API_KEY"]"
+MY_API_KEY = st.secrets["GOOGLE_API_KEY"]
 
 try:
     genai.configure(api_key=MY_API_KEY)
